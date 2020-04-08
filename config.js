3466f3d2e109374bcd150500b8c5c2fd16ae529e
Girma Nigusse <xgirma@gmail.com>
2020-04-08 06:41:49 -0400

    updates

--- config.js
+++ config.js
@@ -1,22 +1,19 @@
 /* eslint-disable global-require */
 
-function config(url, browser) {
+async function config(url, browser) {
   return process.env.GITHUB_ACTIONS
     ? browser
         .init({
           browserName: "chrome",
           "goog:chromeOptions": {
             args: ["--headless", "--disable-gpu"],
-            binary: require("puppeteer").executablePath(),
           },
         })
         .get(url)
     : browser
         .init({
           browserName: "chrome",
-          "goog:chromeOptions": {
-            binary: require("puppeteer").executablePath(),
-          },
+          "goog:chromeOptions": {},
         })
         .get(url);
 }
